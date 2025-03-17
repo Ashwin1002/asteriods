0e825d91fcfb33755e72dc49eb23fa2b8fd114f8
Ashwin1002 <ashwin.shrestha2258@gmail.com>
2025-03-18 01:00:11 +0545

    player gestures/key-mapping updated: - added player related constants - created class player inherited from circleshape - drawn player as triangle - added rotate, move and update function according to WSAD keys - divided player movement and drawable into group

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # limiting key event update to updatable group
+    updatable = pygame.sprite.Group()
+
+    # limiting shape drawable group
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    dt = 0
     pygame.init()
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        updatable.update(dt)
+
         pygame.Surface.fill(screen, (0,0,0))
+        
+        for obj in drawable:
+            obj.draw(screen)
+            
         pygame.display.flip()
+        dt = clock.tick(60) / 1000 
 
 if __name__ == "__main__":
     main()
